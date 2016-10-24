c7a595b7abb621da1a298748cd4d5650b60f5c69
ecmjohnson <erikjohnson@cmail.carleton.ca>
2016-10-24 00:38:05 -0400

    First commit working on the careerjet search engine and system architecture

---
+++ careerjet_search.py
@@ -0,0 +1,22 @@
+from careerjet_api_client import CareerjetAPIClient
+
+class CareerJet:
+
+    def __init__(self, secret, ip_addr, user_agent, locale_code):
+        self.affid = secret
+        self.ip = ip_addr
+        self.agent = user_agent
+        self.cj  =  CareerjetAPIClient(locale_code);
+
+    def search(self, location, keywords, url):
+        # returns a dict with keys:
+        #      type, hits, jobs, pages, response_time
+        result_json = self.cj.search({
+                                'location'    : location,
+                                'keywords'    : keywords,
+                                'affid'       : self.affid,
+                                'user_ip'     : self.ip,
+                                'url'         : url,
+                                'user_agent'  : self.agent
+                              });
+        return(result_json['jobs'])
---
+++ find_my_job.py
@@ -0,0 +1,12 @@
+import json
+from careerjet_search import CareerJet
+
+# get the secret keys for using the apis
+with open('secrets.ids') as f:
+    ids = json.loads(f.read())
+
+careerjet = CareerJet(ids['careerjet'],
+    'ip_goes_here',
+    'Mozilla',
+    'en_GB')
+print(careerjet.search('berlin', 'robotics', 'http://www.example.com/jobsearch?q=python&l=london'))
