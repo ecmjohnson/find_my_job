3bf13b3e6a0743ef185ce755b92301ffeea50cd5
ecmjohnson <erikjohnson@cmail.carleton.ca>
2016-10-25 19:19:22 -0400

    Enabled searching of multiple locations using a json input file

--- find_my_job.py
+++ find_my_job.py
@@ -6,34 +6,48 @@ from job import Job
 with open('secrets.ids') as f:
     ids = json.loads(f.read())
 
-# search careerjet using their api
-jobs = []
-careerjet = CareerJet(
-    ids['careerjet'],
-    'ip_goes_here',
-    'Mozilla',
-    'en_GB'
-)
-search = careerjet.search('zurich', 'robotics', 'http://www.example.com/')
+# get the list of locations and keywords to search
+with open('where_what.json') as f:
+    where_what = json.loads(f.read())
 
-# check success of search
-if search != -1:
-    # build a list of job objects for comparison
-    for job in search:
-        try:
-            salary = int(job['salary_min'])
-            currency = job['salary_currency_code']
-        except KeyError, ValueError:
-            salary = -1
-            currency = 'None'
-        jobs.append(Job(
-            job['title'],
-            job['description'],
-            salary,
-            currency,
-            job['company'],
-            'zurich',
-            job['url']
-        ))
-else:
-    pass  # should probably notify user
+# for every user desired location, keyword pair
+jobs = []
+for location in where_what.keys():
+    # search careerjet using their api
+    careerjet = CareerJet(
+        ids['careerjet'],
+        'ip_goes_here',
+        'Mozilla',
+        'en_GB'
+    )
+    search = careerjet.search(location, where_what[location], 'http://www.example.com/')
+
+    # check success of search
+    if search != -1:
+        # build a list of job objects for comparison
+        for job in search:
+            try:
+                salary = int(job['salary_min'])
+                currency = job['salary_currency_code']
+            except KeyError, ValueError:
+                salary = -1
+                currency = 'None'
+            jobs.append(Job(
+                job['title'],
+                job['description'],
+                salary,
+                currency,
+                job['company'],
+                'zurich',
+                job['url']
+            ))
+    else:
+        pass  # should probably notify user
+
+# now we need to evaluate all the jobs based on the users interest
+for job in jobs:
+    job.compute_value()
+
+# order the jobs by their computed value
+jobs.sort(key=lambda x: x.value, reverse=True)
+import ipdb; ipdb.set_trace()
