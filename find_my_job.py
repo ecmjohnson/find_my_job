de8fd3711bb285c8bacf39be10e83724c2baab9c
ErikJohnsonCU <erikjohnson@cmail.carleton.ca>
2016-11-03 18:06:07 -0400

    Included a timestamp and allowed searching of multiple domains from where_what

--- find_my_job.py
+++ find_my_job.py
@@ -1,8 +1,10 @@
-import json
-from careerjet_search import CareerJet
-from job import Job
+import json  # for the json
+from datetime import datetime  # for the dates and times
+import pickle  # for the pickling
+from careerjet_search import CareerJet  # for the jobs
+from job import Job  # for representing the jobs
 
-# get the secret keys for using the apis
+# get the secret keys for using the api
 with open('secrets.ids') as f:
     ids = json.loads(f.read())
 
@@ -16,37 +18,41 @@ with open('interests.json') as f:
 
 # for every user desired location, keyword pair
 jobs = []
-for location in where_what.keys():
-    # search careerjet using their api
-    careerjet = CareerJet(
-        ids['careerjet'],
-        'ip_goes_here',
-        'Mozilla',
-        'en_GB'
-    )
-    search = careerjet.search(location, where_what[location], 'http://www.example.com/')
+for domain in where_what.keys():
+    for location in where_what[domain].keys():
+        # search careerjet using their api
+        careerjet = CareerJet(
+            ids['careerjet'],
+            'ip_goes_here',
+            'Mozilla',
+            domain
+        )
+        search = careerjet.search(location,
+                                  where_what[domain][location],
+                                  'http://www.example.com/')
 
-    # check success of search
-    if search != -1:
-        # build a list of job objects for comparison
-        for job in search:
-            try:
-                salary = int(job['salary_min'])
-                currency = job['salary_currency_code']
-            except KeyError, ValueError:
-                salary = -1
-                currency = 'None'
-            jobs.append(Job(
-                job['title'],
-                job['description'],
-                salary,
-                currency,
-                job['company'],
-                'zurich',
-                job['url']
-            ))
-    else:
-        pass  # should probably notify user
+        # check success of search
+        if search != -1:
+            # build a list of job objects for comparison
+            for job in search:
+                try:
+                    salary = int(job['salary_min'])
+                    currency = job['salary_currency_code']
+                except KeyError, ValueError:
+                    salary = -1
+                    currency = 'None'
+                jobs.append(Job(
+                    job['title'],
+                    job['description'],
+                    salary,
+                    currency,
+                    job['company'],
+                    'zurich',
+                    job['url'],
+                    datetime.now()
+                ))
+        else:
+            pass  # should probably notify user
 
 # now we need to evaluate all the jobs based on the users interest
 for job in jobs:
@@ -54,4 +60,7 @@ for job in jobs:
 
 # order the jobs by their computed value
 jobs.sort(key=lambda x: x.value, reverse=True)
+
+# pickle the list so we can read it back later to compare
+
 import ipdb; ipdb.set_trace()
